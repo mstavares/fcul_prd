1c43867acc4fe97b544eb98dee140c6e3c34e4ee
mstavares <hmigueltavares@gmail.com>
2018-04-04 19:51:26 +0100

    Added arguments which sets bw on the commnand line

--- PA2/Tree.py
+++ PA2/Tree.py
@@ -9,26 +9,33 @@ from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
-class Tree(Topo):
-    "Simple Data Center Topology"
-    "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
-    "fanout - number of child switch per parent switch"
+# Core switch is the first level of our tree architecture
+CORE_LEVEL = 0
+# Access switches are the second level of our tree architecture
+ACCESS_LEVEL = 1
+# Edge switches are the third level of our tree architecture
+EDGE_LEVEL = 2
+# Hosts are the fourth level of our tree architecture
+HOST_LEVEL = 3
 
-    def __init__(self, fanout, **opts):
+
+class Tree(Topo):
+
+    def __init__(self, alopts, elopts, hlopts, fanout, **opts):
         # Initialize topology and default options
         Topo.__init__(self, **opts)
         # Adds core switches
-        self.createSwitches('cs', fanout, 0)
+        self.createSwitches('cs', fanout, CORE_LEVEL)
         # Adds aggregation switches
-        self.createSwitches('as', fanout, 1)
+        self.createSwitches('as', fanout, ACCESS_LEVEL)
         # Adds edge switches
-        self.createSwitches('es', fanout, 2)
+        self.createSwitches('es', fanout, EDGE_LEVEL)
         # Adds hosts
-        self.createHosts('h', fanout, 3)
+        self.createHosts('h', fanout, HOST_LEVEL)
         # Adds links
-        self.createLinks('cs', 'as', fanout, 1)
-        self.createLinks('as', 'es', fanout, 2)
-        self.createLinks('es', 'h', fanout, 3)
+        self.createLinks('cs', 'as', alopts, fanout, ACCESS_LEVEL)
+        self.createLinks('as', 'es', elopts, fanout, EDGE_LEVEL)
+        self.createLinks('es', 'h', hlopts, fanout, HOST_LEVEL)
 
     # Create switches
     def createSwitches(self, label, fanout, level):
@@ -41,34 +48,45 @@ class Tree(Topo):
             self.addHost(label + str(x))
 
     # Create links
-    def createLinks(self, abovelabel, thislabel, fanout, level):
+    def createLinks(self, abovelabel, thislabel, linkopts, fanout, level):
         for x in range(int(pow(fanout, level - 1))):
             for y in range(fanout * x, fanout * (x + 1)):
                 aboveSwich = abovelabel + str(x)
                 thisSwitch = thislabel + str(y)
-                self.addLink(aboveSwich, thisSwitch)
+                self.addLink(aboveSwich, thisSwitch, **linkopts)
 
 
-
-def perfTest(fanout=2):
+def perfTest(alopts, elopts, hlopts, fanout):
     "Create network and run simple performance test"
-    topo = Tree(fanout)
+    topo = Tree(alopts, elopts, hlopts, fanout)
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, cleanup=True)
     net.start()
     print "Dumping host connections"
-    dumpNodeConnections( net.hosts )
+    dumpNodeConnections(net.hosts)
     print "Testing network connectivity"
     net.pingAll()
-    print "Testing bandwidth between h1 and h4"
-    h1, h2 = net.get( 'h1', 'h2' )
-    net.iperf( (h1, h2) )
+    last_host_id = pow(fanout, HOST_LEVEL) - 1
+    print "Testing bandwidth between h0 and h%d" % last_host_id
+    hs, hc = net.get('h0', 'h%d' % last_host_id)
+    net.iperf((hs, hc))
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel('info')
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-f", "--fanout", required=True, help="fanout number")
+    # fanout argument
+    ap.add_argument("-f", "--fanout", default=2, required=False, help="fanout number")
+    # access link throughput argument
+    ap.add_argument("-al", "--accessLink", default=20, required=False, help="access link throughput")
+    # edge link throughput argument
+    ap.add_argument("-el", "--edgeLink", default=10, required=False, help="edge link throughput")
+    # host link throughput argument
+    ap.add_argument("-hl", "--hostLink", default=1, required=False, help="host link throughput")
     args = vars(ap.parse_args())
+    alopts = dict(bw=int(args["accessLink"]))
+    elopts = dict(bw=int(args["edgeLink"]))
+    hlopts = dict(bw=int(args["hostLink"]))
     fanout = int(args["fanout"])
-    perfTest(fanout)
+    perfTest(alopts, elopts, hlopts, fanout)
